c46ff9be23da3edbbf5b9d52822d52a22543b4b1
Kristofer Lindberg <k.lindberg@gmail.com>
2013-08-06 23:24:21 +0200

    separate shots

--- main.lua
+++ main.lua
@@ -6,11 +6,16 @@ function love.load()
    ship = love.graphics.newImage("ship.png")
    ship:setFilter("nearest", "nearest")
 
+   width = love.graphics.getWidth()
+   height = love.graphics.getHeight()
+
    sx = 392 -- not exactly middle so that we don't rotate around ship tip
    sy = 292
 
    angle = 0
    rotf = 40
+
+   shots = {}
 end
 
 function love.keypressed(key, unicode)
@@ -24,22 +29,40 @@ function love.keypressed(key, unicode)
       angle = angle - math.pi / rotf
    end
    if key == " " then
-      fire_laser = true
+      shoot()
    end
 end
 
 function love.update(dt)
+   local rem_shot = {}
    if love.keyboard.isDown("left") then
       angle = angle + math.pi / rotf
    end
    if love.keyboard.isDown("right") then
       angle = angle - math.pi / rotf
    end
+
+   for i,v in ipairs(shots) do
+      v.y = v.y - dt*100
+      --if v.y < 0 or v.y > height or v.x < 0 or v.x > width then
+      if v.y < -200 then -- since we are rotating and x is larger than y this have to be negative atm
+	 table.insert(rem_shot, i)
+      end
+   end
+
+   for i,v in ipairs(rem_shot) do
+      table.remove(shots, v)
+   end
 end
 
 function love.draw()
    rotate_graphics(ship, angle, sx, sy)
-   if fire_laser then draw_fire_laser(400,300,400,300-30)end
+   love.graphics.setColor(255,255,255,255)
+   for i,v in ipairs(shots) do
+      --love.graphics.rectangle("fill", v.x, v.y, 2, 5)
+      draw_fire_laser(v.x, v.y, v.x, v.y-10, v.a)
+   end
+   --if fire_laser then draw_fire_laser(400,300,400,300-30)end
 end
 
 function draw_scaled_graphics(g, s, x, y)
@@ -57,8 +80,6 @@ function print_scaled_graphics(t, s, x, y)
 end
 
 function rotate_graphics(g, a, gx, gy)
-   local width = love.graphics.getWidth()
-   local height = love.graphics.getHeight()
    love.graphics.push()
    love.graphics.translate(width/2, height/2)
    love.graphics.rotate(a)
@@ -67,17 +88,24 @@ function rotate_graphics(g, a, gx, gy)
    love.graphics.pop()
 end
 
-function draw_fire_laser(x1,y1,x2,y2)
+function draw_fire_laser(x1,y1,x2,y2,a)
    love.graphics.setColor(255, 0, 0)
    love.graphics.setLine(2, "smooth")
-   local width = love.graphics.getWidth()
-   local height = love.graphics.getHeight()
    love.graphics.push()
    love.graphics.translate(width/2, height/2)
-   love.graphics.rotate(angle)
+   love.graphics.rotate(a)
    love.graphics.translate(-width/2, -height/2)
-   love.graphics.line(x1,y1,x2,y2)
+   --love.graphics.line(x1,y1,x2,y2)
+   love.graphics.rectangle("fill",x1,y1,2,5)
    love.graphics.pop()
    love.graphics.reset()
-   fire_laser = false
+   --fire_laser = false
+end
+
+function shoot()
+   local shot = {}
+   shot.x = sx
+   shot.y = sy
+   shot.a = angle
+   table.insert(shots, shot)
 end
