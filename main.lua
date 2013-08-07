4946a097e2988f6961d50cd1753f0b2b23fe49e3
Kristofer Lindberg <k.lindberg@gmail.com>
2013-08-07 22:43:51 +0200

    collisions and enemies

--- main.lua
+++ main.lua
@@ -23,6 +23,8 @@ function love.load()
    ship.h = ship.i:getHeight()
 
    shots = {}
+
+   enemies = {}
 end
 
 function love.keypressed(key, unicode)
@@ -32,10 +34,14 @@ function love.keypressed(key, unicode)
    if key == " " then
       shoot()
    end
+   if key == "e" then
+      spawn()
+   end
 end
 
 function love.update(dt)
    local rem_shot = {}
+   local rem_enemy = {}
    if love.keyboard.isDown("left") then
       if ship.x > 0 then
 	 ship.x = ship.x - 1
@@ -62,20 +68,41 @@ function love.update(dt)
       if v.y < 0 then
 	 table.insert(rem_shot, i)
       end
+
+      for ii,vv in ipairs(enemies) do
+	 if check_collision(v.x,v.y,2,5,vv.x,vv.y,15,15) then
+	    table.insert(rem_enemy, ii)
+	    table.insert(rem_shot, i)
+	 end
+      end
    end
 
    for i,v in ipairs(rem_shot) do
       table.remove(shots, v)
+   end
+   for i,v in ipairs(rem_enemy) do
+      table.remove(enemies, v)
+   end
+
+   for i,v in ipairs(enemies) do
+      v.y = v.y + dt
    end
 end
 
 function love.draw()
    love.graphics.setBackgroundColor( 0, 0, 0 )
    love.graphics.draw(ship.i, ship.x, ship.y)
+   
    love.graphics.setColor(255,0,0)
-   for i,v in ipairs(shots) do
+   for _,v in ipairs(shots) do
       love.graphics.rectangle("fill", v.x, v.y, 2, 5)
    end
+   
+   love.graphics.setColor(255,120,120)
+   for _,v in ipairs(enemies) do
+      love.graphics.rectangle("fill", v.x, v.y, 15, 15)
+   end
+   
    love.graphics.reset()
 end
 
@@ -98,4 +125,16 @@ function shoot()
    shot.x = ship.x + ship.w/2
    shot.y = ship.y
    table.insert(shots, shot)
+end
+
+function spawn()
+   local enemy = {}
+   enemy.x = math.random(0, width)
+   enemy.y = math.random(0, height/2)
+   table.insert(enemies, enemy)
+end
+
+function check_collision(ax1,ay1,aw,ah, bx1,by1,bw,bh)
+   local ax2,ay2,bx2,by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
+   return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
 end
