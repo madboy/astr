7c0b5e67cfe5710a7f2346ea5495fe504fbee060
Kristofer Lindberg <k.lindberg@gmail.com>
2013-08-18 09:43:48 +0200

    adding logo and state

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
-game = {}
-
 function love.load()
    -- graphics
-   img_fn = {"ship", "ship_2", "ship_3", "ship_4", "shot", "monster", "background"}
+   img_fn = {"ship", "ship_2", "ship_3", "ship_4", "shot", "monster", "background", "logo", "explosion_v2_7"}
    imgs = {}
    for _,v in ipairs(img_fn) do
       imgs[v] = love.graphics.newImage("assets/"..v..".png")
@@ -28,126 +26,144 @@ function love.load()
    score = 0
    fired = 0
 
+   game = {}
    game.clock = 0
    game.enemy_dt = 0
    game.enemy_rate = 2
    game.enemy_size = imgs["monster"]:getWidth()
    game.player_size = imgs["ship"]:getWidth()
    game.shot_size = imgs["shot"]:getWidth()
+   game.logo_size = imgs["logo"]:getWidth()
 
    debug = false
 
    -- sounds
    shot_sound = love.audio.newSource("assets/shot.ogg", "static")
    hit_sound = love.audio.newSource("assets/hit.ogg", "static")
+
+   state = "logo"
 end
 
 function love.keypressed(key, unicode)
    if key == "escape" then
       love.event.push("quit")
    end
-   if key == " " then
-      fired = fired + 1
-      shoot()
-   end
-   if key == "e" then
-      spawn()
-   end
-   if key == "`" then
-      debug = not debug
+   if state == "logo" then
+      if key == "return" then
+	 state = "game"
+      end
+   elseif state == "game" then
+      if key == " " then
+	 fired = fired + 1
+	 shoot()
+      end
+      if key == "e" then
+	 spawn()
+      end
+      if key == "`" then
+	 debug = not debug
+      end
    end
 end
 
 function love.update(dt)
-   game.clock = game.clock + dt
-   game.enemy_dt = game.enemy_dt + dt
+   if state == "logo" then
+      return
+   elseif state == "game" then
+      game.clock = game.clock + dt
+      game.enemy_dt = game.enemy_dt + dt
 
-   if game.enemy_dt > game.enemy_rate then
-      spawn()
-      game.enemy_dt = 0
-   end
-
-   local rem_shot = {}
-   local rem_enemy = {}
-   if love.keyboard.isDown("left") then
-      if ship.x > 0 then
-	 ship.x = ship.x - 100*dt*scale
-      end
-   end
-   if love.keyboard.isDown("right") then
-      if ship.x + game.player_size < 160*scale then
-	 ship.x = ship.x + 100*dt*scale
-      end
-   end
-   if love.keyboard.isDown("up") then
-      if ship.y > 0 then
-	 ship.y = ship.y - 100*dt*scale
-      end
-   end
-   if love.keyboard.isDown("down") then
-      if ship.y + game.player_size < 144*scale then
-	 ship.y = ship.y + 100*dt*scale
-      end
-   end
-
-   for i,v in ipairs(shots) do
-      v.y = v.y - dt*50*scale
-      if v.y < 0 then
-	 table.insert(rem_shot, i)
+      if game.enemy_dt > game.enemy_rate then
+	 spawn()
+	 game.enemy_dt = 0
       end
 
-      for ii,vv in ipairs(enemies) do
-	 if distance(v.x,v.y, vv.x, vv.y) < 10*scale then
-	    love.audio.play(hit_sound)
-	    table.insert(rem_enemy, ii)
-	    table.insert(rem_shot, i)
-	    score = score + 1
+      local rem_shot = {}
+      local rem_enemy = {}
+      if love.keyboard.isDown("left") then
+	 if ship.x > 0 then
+	    ship.x = ship.x - 100*dt*scale
 	 end
       end
-   end
+      if love.keyboard.isDown("right") then
+	 if ship.x + game.player_size < 160*scale then
+	    ship.x = ship.x + 100*dt*scale
+	 end
+      end
+      if love.keyboard.isDown("up") then
+	 if ship.y > 0 then
+	    ship.y = ship.y - 100*dt*scale
+	 end
+      end
+      if love.keyboard.isDown("down") then
+	 if ship.y + game.player_size < 144*scale then
+	    ship.y = ship.y + 100*dt*scale
+	 end
+      end
 
-   for i,v in ipairs(rem_shot) do
-      table.remove(shots, v)
-   end
-   for i,v in ipairs(rem_enemy) do
-      table.remove(enemies, v)
-   end
+      for i,v in ipairs(shots) do
+	 v.y = v.y - dt*50*scale
+	 if v.y < 0 then
+	    table.insert(rem_shot, i)
+	 end
 
-   for i,v in ipairs(enemies) do
-      v.y = v.y + 20*dt*scale
+	 for ii,vv in ipairs(enemies) do
+	    if distance(v.x,v.y, vv.x, vv.y) < 10*scale then
+	       love.audio.play(hit_sound)
+	       table.insert(rem_enemy, ii)
+	       table.insert(rem_shot, i)
+	       score = score + 1
+	    end
+	 end
+      end
+
+      for i,v in ipairs(rem_shot) do
+	 table.remove(shots, v)
+      end
+      for i,v in ipairs(rem_enemy) do
+	 table.remove(enemies, v)
+      end
+
+      for i,v in ipairs(enemies) do
+	 v.y = v.y + 20*dt*scale
+      end
    end
 end
 
 function love.draw()
-   for i = 0,4 do
-    for j = -1,4 do
-      love.graphics.draw(imgs["background"],
-                           i*32*scale,
-                           (j+game.clock%1)*32*scale,
-                           0,scale,scale)
-    end
-   end
+   if state == "logo" then
+      love.graphics.draw(imgs["logo"], width/2, height/2, 0, scale, scale, game.logo_size/2, game.logo_size/2)
+   elseif state == "game" then
+      for i = 0,4 do
+	 for j = -1,4 do
+	    love.graphics.draw(imgs["background"],
+			       i*32*scale,
+			       (j+game.clock%1)*32*scale,
+			       0,scale,scale)
+	 end
+      end
 
-   for _,v in ipairs(enemies) do
-      love.graphics.draw(imgs["monster"], v.x, v.y, 0, scale, scale, game.enemy_size/2, game.enemy_size/2)
-      if debug then love.graphics.circle("line",v.x,v.y,game.enemy_size/2*scale) end
-   end
+      for _,v in ipairs(enemies) do
+	 love.graphics.draw(imgs["monster"], v.x, v.y, 0, scale, scale, game.enemy_size/2, game.enemy_size/2)
+	 if debug then love.graphics.circle("line",v.x,v.y,game.enemy_size/2*scale) end
+      end
 
-   local ship_image = ship.i[ship.idx]
-   love.graphics.draw(ship_image, ship.x, ship.y, 0, scale, scale, game.player_size/2, game.player_size/2)
-   ship.idx = ship.idx + 1
-   if ship.idx > table.getn(ship.i) then
-      ship.idx = 1
-   end
+      local ship_image = ship.i[ship.idx]
+      love.graphics.draw(ship_image, ship.x, ship.y, 0, scale, scale, game.player_size/2, game.player_size/2)
+      ship.idx = ship.idx + 1
+      if ship.idx > table.getn(ship.i) then
+	 ship.idx = 1
+      end
 
-   for _,v in ipairs(shots) do
-      love.graphics.draw(imgs["shot"], v.x, v.y, 0, scale, scale, game.shot_size/2, game.shot_size/2)
-      if debug then love.graphics.circle("line",v.x,v.y,game.shot_size/2*scale) end
+      for _,v in ipairs(shots) do
+	 love.graphics.draw(imgs["shot"], v.x, v.y, 0, scale, scale, game.shot_size/2, game.shot_size/2)
+	 if debug then love.graphics.circle("line",v.x,v.y,game.shot_size/2*scale) end
+      end
+      
+      love.graphics.setColor(255,215,0)
+      love.graphics.printf("score: "..score.." shots: "..fired, 0,0, width, "center")
+      love.graphics.setColor(255,255,255)
    end
-   
-   love.graphics.setColor(255,215,0)
-   love.graphics.printf("score: "..score.." shots: "..fired, 0,0, width, "center")
-   love.graphics.setColor(255,255,255)
 end
 
 function shoot()
